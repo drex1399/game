de9838aa7f770bfe77f51edd376953d7edc3f357
EH <evan.holgate@connect.7oaks.org>
2020-08-13 15:49:51 -0500

    add player jump animation

--- Assets/scripts/playercontroller.cs
+++ Assets/scripts/playercontroller.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class playercontroller : MonoBehaviour
 {
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +14,17 @@ public class playercontroller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      
+
+
+       if(Input.GetKeyDown(KeyCode.Space))
+        {
+
+        anim.SetTrigger("Jump");
+        }
     }
 }
+
+
+
+
